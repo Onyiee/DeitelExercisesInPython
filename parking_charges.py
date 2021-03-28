7ea8178dd30fca084f2bc074247cbe8bbf5fce42
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-03-28 01:10:05 +0100

    updated calculate charges method

--- parking_charges.py
+++ parking_charges.py
@@ -7,9 +7,11 @@
 # display the running total of yesterday’s receipts. It should use the method calculateCharges to
 # determine the charge for each customer
 
+
 class Car_park_charges_calculator:
     hours_parked = 0
-    fee = 0.50
+    charges_per_hour = 0.50
+    total_charges = 0
 
     def set_hours_parked(self, hours_parked):
         self.hours_parked = hours_parked
@@ -17,13 +19,47 @@ class Car_park_charges_calculator:
     def get_hours_parked(self):
         return self.hours_parked
 
-    def set_fee_for_hours_parked(self, fee):
-        if self.hours_parked == 3:
-            self.fee = 2.00
-        else:
-            self.fee = fee
+    def set_fee_for_hours_parked(self, charges_per_hour):
+        self.charges_per_hour = charges_per_hour
 
     def get_fee_for_hours_parked(self):
-        return self.fee
+        return self.charges_per_hour
 
-    def calculate_charges(self,hours_parked,fee):
+    def calculate_charges(self, hours_parked):
+        # total_charges = hours_parked * self.charges_per_hour
+        # customers = []
+        # self.set_fee_for_hours_parked()
+        if hours_parked == 3:
+            self.total_charges = 2.00
+            print(self.total_charges)
+            return self.total_charges
+
+        if hours_parked < 3:
+            self.charges_per_hour = 0.67
+            self.total_charges = hours_parked * self.charges_per_hour
+            return self.total_charges
+
+        if hours_parked > 3 and hours_parked < 24:
+            extra_hours_parked = hours_parked - 3
+            self.total_charges = 2.00
+            self.charges_per_hour = 0.5
+            extra_fee = extra_hours_parked * self.charges_per_hour
+            self.total_charges = extra_fee + self.total_charges
+            return self.total_charges
+        if hours_parked == 24:
+            self.total_charges = 10.00
+            return self.total_charges
+
+    def get_charges(self):
+        return self.total_charges
+        # self.get_fee_for_hours_parked()
+        # self.set_hours_parked(4)
+        # self.get_fee_for_hours_parked()
+        # total_charges = hours_parked * self.charges_per_hour
+        # print(total_charges)
+
+        # charges_per_hour = 0.50
+        # for i in customers:
+        # hours_parked = int(input("Enter the number of hours the car was parked for: "))
+        # total_charges = hours_parked * self.charges_per_hour
+        # print(f"the total charges to be paid is {total_charges}")
