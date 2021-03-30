d594e27a780c9f312c8949790950f5af4f88e6ec
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-03-30 10:54:51 +0100

    refactored

--- Multiples.py
+++ Multiples.py
@@ -4,11 +4,12 @@
 # Incorporate this method into an application that inputs a series of pairs of integers (one pair at a
 # time) and determines whether the second value in each pair is a multiple of the first.
 
-def is_multiple():
-    first_number = int(input("Enter an integer: "))
-    second_number = int(input("Enter another number: "))
+def is_multiple(first_number, second_number):
+    # first_number = int(input("Enter an integer: "))
+    # second_number = int(input("Enter another number: "))
     if second_number % first_number == 0:
         return True
+
     else:
         return False
 
