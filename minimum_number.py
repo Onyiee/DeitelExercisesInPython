6503a3d746640af3111454007f09e2796837b0ae
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-05 19:47:58 +0100

    refactored

--- minimum_number.py
+++ minimum_number.py
@@ -4,24 +4,10 @@
 # the result.
 
 
-def minimum3(first_number, second_number, third_number):
-    if first_number < second_number and first_number < third_number:
-        minimum_number = first_number
-        return minimum_number
-    if second_number < first_number and second_number < third_number:
-        minimum_number = second_number
-        return minimum_number
-    if third_number < first_number and third_number < second_number:
-        minimum_number = third_number
-        return minimum_number
+def minimum3(numbers):
+    min_number = numbers[0]
+    for i in numbers:
+        if i < min_number:
+            min_number = i
+    return min_number
 
-
-if __name__ == '__main__':
-    try:
-        first_number = float(input("Enter a number: "))
-        second_number = float(input("Enter a second number: "))
-        third_number = float(input("Enter a third number: "))
-        min_number = (minimum3(first_number, second_number, third_number))
-        print(f"The minimum number is {min_number}")
-    except ValueError:
-        print("This program accepts only numbers.Enter a number.")
