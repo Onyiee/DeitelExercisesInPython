5decc2517be92a4e8e786283dc3e3372ccfe4d25
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-06 11:55:47 +0100

    updated test

--- test_minimum_number.py
+++ test_minimum_number.py
@@ -4,5 +4,7 @@ from minimum_number import minimum3
 
 class Test_minimum_number(TestCase):
     def test_minimum3(self):
-        minimum3(3, 8, 1)
-        self.assertEqual(1, minimum3(3, 8, 1))
+        list_of_numbers = [9, 1, 2, 3, 4, 5]
+        minimum3(list_of_numbers)
+        self.assertEqual(1, minimum3(list_of_numbers))
+        self.assertNotEqual(2, minimum3(list_of_numbers))
