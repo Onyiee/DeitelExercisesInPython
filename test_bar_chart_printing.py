df58741138a367c4a41170217bf2dc700c31dcdd
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-05-15 22:11:10 +0100

    refactored

--- test_bar_chart_printing.py
+++
@@ -1,8 +0,0 @@
-# from unittest import TestCase
-# from bar_chart_printing import display_bar_chart
-#
-#
-# class Test_bar_chart_printing(TestCase):
-#     def test_display_bar_chart(self):
-#
-#         self.assertEqual(5,  display_bar_chart)
